168dcae50b288b159e2d8b87290509cd1be2daf8
MaximeBoinet <maximeboinet@live.com>
2015-11-10 12:38:57 +0100

    La meilleur version

--- EXO.php
+++ EXO.php
@@ -1,20 +1,24 @@
 <?php
 
 $tab = array();
-
+$cpt = 0;
 for($i=0; $i<100, i++)
 {
 	$tab[$i]=rand(0,100);
 
 }
 
-foreach ($var in $tab) 
-
+for ($i = 0; $i<100, i++)
 {
- 
-
- 
+	foreach ($var in $tab) 
+	{
+		if ($var == $i}
+		{
+			$cpt ++;
+		}
+	}
+	echo ('La valeur $i est presente $ctp fois');
+	$cpt =0;
 }
-
 
 ?>
